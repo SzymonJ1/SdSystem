809fdeadedb57933b19c10d9aec4460cf5a9e97d
Przemek <jancewicz.przemek@gmail.com>
2024-07-12 18:01:36 +0200

    Dodałem przycisk 'panel koordynatora': - wpisanie danych do logowania - zatwierdzenie danych - przekierowanie do zakłądki 'tickets'

---
+++ SDsystem/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDsystem.Models;
+
+namespace SDsystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly string adminUsername = "admin";
+        private readonly string adminPassword = "admin";
+
+        // GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserModel model)
+        {
+            if (model.Username == adminUsername && model.Password == adminPassword)
+            {
+                return RedirectToAction("Index", "Tickets");
+            }
+            else
+            {
+                ViewBag.Message = "Nieprawidłowe dane logowania";
+                return View();
+            }
+        }
+    }
+}
---
+++ SDsystem/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SDsystem.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
